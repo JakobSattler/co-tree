261d858b12c7ee2d31db7463492336bad0f6566b
Jakob Sattler <satjab12@htlkaindorf.at>
2016-08-17 12:58:55 +0200

    Change nodetext

--- src/app/tree-node/tree-node.component.ts
+++ src/app/tree-node/tree-node.component.ts
@@ -1,4 +1,13 @@
-import {Component, OnInit, Input, Output, EventEmitter, forwardRef, Inject, ViewChild, ElementRef} from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+  AfterViewChecked
+} from '@angular/core';
 import {TreeNode} from '../tree-node';
 import {TreeService} from '../tree.service';
 import {TreeComponent} from '../tree.component';
@@ -11,10 +20,11 @@ import {TreeComponent} from '../tree.component';
   directives: [TreeNodeComponent],
   providers: [TreeService]
 })
-export class TreeNodeComponent implements OnInit {
+export class TreeNodeComponent implements OnInit, AfterViewChecked {
 
   extended: boolean = false;
   paddingPerLevel: number = 18;
+  changing: boolean = false;
 
   @Input()
   level: number;
@@ -28,14 +38,23 @@ export class TreeNodeComponent implements OnInit {
   @Output()
   nodeSelected = new EventEmitter();
 
+  @ViewChild('nodeTextInput')
+  nodeTextInput: ElementRef;
+
   @ViewChild('nodeText')
   nodeText: ElementRef;
 
-  constructor(private treeService: TreeService, @Inject(forwardRef(() => TreeComponent)) private treeComponent: TreeComponent) {
+  constructor(private treeService: TreeService) {
   }
 
   ngOnInit() {
     this.node.level = this.level;
+  }
+
+  ngAfterViewChecked() {
+    if(this.changing){
+      this.nodeTextInput.nativeElement.focus();
+    }
   }
 
   extend() {
@@ -53,36 +72,30 @@ export class TreeNodeComponent implements OnInit {
 
   changePic() {
 
-    if(!(this.classString = prompt("Change Pic", "change pic here")))
-    {
+    if (!(this.classString = prompt("Change Pic", "change pic here"))) {
       this.classString = 'http://www.iconarchive.com/download/i83780/pelfusion/flat-folder/Close-Folder.ico';
-    };
+    }
+    ;
   }
 
   editNode() {
-    this.nodeText.nativeElement.focus();
+    this.changing = true;
   }
 
   onKeyDown(event) {
-    console.log(event.keyCode);
-
-    //handle text change if source of event is nodeText-element
-    if (event.srcElement == this.nodeText.nativeElement) {
+    //handle text change if source of event is nodeTextInput-element
+    if (event.srcElement == this.nodeTextInput.nativeElement) {
       if (event.keyCode == 13) {
         this.saveNodeChange();
-      } else if (event.keyCode >= 64 && event.keyCode <= 90) {
-        this.node.name += String.fromCharCode(event.keyCode);
       }
     }
+
   }
 
   saveNodeChange() {
-    this.nodeText.nativeElement.blur();
-    console.log('save!');
-  }
-
-  discardNodeChange() {
-    console.log('discard');
+    this.nodeTextInput.nativeElement.blur();
+    this.node.name = this.nodeTextInput.nativeElement.value;
+    this.changing = false;
   }
 
   deleteNode() {
@@ -91,5 +104,7 @@ export class TreeNodeComponent implements OnInit {
       alert("Delete");
     }
   }
+
+
 
 }
